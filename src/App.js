fe74889c29d2456b4cde9bfe72c1b519de31b825
chetankonnur <40333836+chetankonnur@users.noreply.github.com>
2019-06-07 12:17:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import MenuAppBar from "./AppbarComponent";
+
+class App extends React.Component {
+  render() {
+    return <MenuAppBar />;
+  }
+}
+
+export default App;
---
+++ src/AppbarComponent.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+
+class MenuAppBar extends Component {
+  render() {
+    const classes = {
+      root: {
+        flexGrow: 1
+      },
+      menuButton: {
+        marginRight: 20
+      },
+      title: {
+        flexGrow: 1
+      },
+      accountCircleIcon: { position: "absolute", right: 0 }
+    };
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="Menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Photos
+            </Typography>
+            <div style={classes.accountCircleIcon}>
+              <IconButton
+                aria-label="Account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={this.handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={this.anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right"
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right"
+                }}
+                open={this.open}
+                onClose={this.handleClose}
+              >
+                <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                <MenuItem onClick={this.handleClose}>My account</MenuItem>
+              </Menu>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+export default MenuAppBar;
